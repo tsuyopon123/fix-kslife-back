554c65744ca9e583f3af8684cca4ad40289c23eb
tsuyopon123 <tsu001r@gmail.com>
2021-04-15 09:41:19 +0900

    first commit

---
+++ back.js
@@ -0,0 +1,14 @@
+// ブラウザバックのアクションが実行された際に、戻るボタンを押す
+
+// disable browser back
+history.pushState(null, null, null);
+
+// 戻るアクションが発火した際に戻るボタンを探してクリックする
+window.addEventListener('popstate', function (e) {
+    if (document.querySelector(".icon-back") != null) {
+        document.querySelector(".icon-back").click();
+    } else {
+        alert('ブラウザバックできません。');
+        history.pushState(null, null, null);
+    }
+});
